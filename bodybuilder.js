c686851071671269c66d99245c929943f4327b44
James McKenzie <jmkenz@gmail.com>
2013-12-16 21:52:52 -0400

    Updates to demo

--- bodybuilder.js
+++ bodybuilder.js
@@ -1,12 +1,19 @@
 /*---- EDITABLE BLOCKS ----*/
 
-/*Start with a single contentEditable p (editable block)*/
+/*Start with a single contenteditable p (editable block)*/
 
 
 /*Enter key creates a new editable block.  Each separate block has a .bbuilder-edit class */
 
 
 /*Shift+Enter creates a (visible) <br> element within current editable block*/
+
+/*List items*/
+$(document).on('click', 'li', function() {
+	$(this).parent('ul').attr('contenteditable', 'false');
+	$(this).attr('contenteditable', 'true').focus();
+});
+
 
 
 /*---- ADDING CLASSES -----*/
@@ -15,7 +22,7 @@
 
 /*---- WIDGETS -----*/
 
-/*Widget blocks get a .bbuilder-widget class, which don't become contentEditable areas. They also get a data-attribute specifying the name of the widget template xml file */
+/*Widget blocks get a .bbuilder-widget class, which don't become contenteditable areas. They also get a data-attribute specifying the name of the widget template xml file */
 
 
 /*---- SAVING THE CONTENT TO A DATABASE -----*/
@@ -25,15 +32,18 @@
 /*---- EDITING EXISTING CONTENT -----*/
 
 /*Parsing the raw HTML back into the editing mode
-	-Block-level HTML elements are given the .bbuilder-edit class and get the contentEditable property
-	.bbuilder-widget elements output their HTML but don't get the contentEditable property. Each element that contains user-specified values has a unique data-field-type value
+	-Block-level HTML elements are given the .bbuilder-edit class and get the contenteditable property
+	.bbuilder-widget elements output their HTML but don't get the contenteditable property. Each element that contains user-specified values has a unique data-field-type value
 	 that tells the widget's modal window to populate the appropriate input field with the right value.
 
-		An element who's data-field-type is set to a nested rich text area only becomes contentEditable when viewed in the modal window.
+		An element who's data-field-type is set to a nested rich text area only becomes contenteditable when viewed in the modal window.
 */
 
 /*---- HTML VIEW -----*/
-/*Isolated HTML view will surround the selected block with <pre><code> tags, but not widget blocks. The markup in widgets stays protected. <pre<code> is stripped back out when
-focus leaves the block
+/*Isolated HTML view will surround the selected block with <pre><code contenteditable="true"> tags, but not widget blocks. The markup in widgets stays protected. contenteditable and the bbuilder-edit class will be stripped from the block's parent element. <pre><code> is stripped back out when focus leaves the block, and the contenteditable attribute is moved back to the block's parent element.
 
-Full HTML view wraps all editable blocks in <pre><code> and only strips them back out when Full HTML view is toggled off.*/
+Carrot characters (< >) get converted to <span class="ct">&lt;</span> and <span class="ct">&gt;</span> while in HTML view. New carrot characters that are typed or pasted in are also converted.  When returning to visual edit mode, those <span class="ct"> elements are convereted back to real carrot characters.
+
+Similarly, special characters get converted to a full code visual: (e.g. &amp; becomes &amp;amp;). 
+
+Full HTML view strips all contenteditable attributes, and then wraps all editable blocks in <pre><code contenteditable="true"> and only strips them back out when Full HTML view is toggled off.  Again, widget blocks are excluded from this.*/
